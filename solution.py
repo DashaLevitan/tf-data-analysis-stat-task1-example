c66f5e7c5673b86952bc9d3fb74cd1e4710019fe
DashaLevitan <90634932+DashaLevitan@users.noreply.github.com>
2023-04-05 16:17:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 808572568
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 76
+    n_cars = x.size
+    a = 2 * x.sum()/(n_cars * t ** 2)
+    return a
